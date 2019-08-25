16e104bc409cf2a288a7406b8f3267a1153fa102
Trung Ly <trung.ly@yoobeecolleges.com>
2019-08-26 10:03:38 +1200

    Router layout template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <div className="app">
+
+
+          <div class="header">
+            <i class="fas fa-plus"></i>
+            <i class="fas fa-bars"></i>
+
+            <ul class="menu">
+              <li><a class="color1" href="">Projects</a></li>
+              <li><a class="color2" href="">Add a project</a></li>
+              <li><a class="color4" href="">Login</a></li>
+              <li><a class="color5" href="">Signup</a></li>
+            </ul>
+          </div>
+
+          <div class="main">
+            <h3>Projects</h3>
+            <div class="card project">
+              <img class="card-img-top" src="project.jpg" alt="Card image cap" />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title</p>
+                <p>
+                  <i class="fas fa-heart"></i>
+                  <i class="fas fa-edit"></i>
+                  <i class="fas fa-trash"></i>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="main">
+            <h3>Add a project</h3>
+            <form>
+              <div class="form-group">
+                <label for="name-input">Name</label>
+                <input type="text" class="form-control" name="name-input" id="name-input" placeholder="Enter project name"/>
+              </div>
+              <div class="form-group">
+                <label for="name-input">Description</label>
+                <input type="text" class="form-control" name="description-input" id="description-input" placeholder="Enter project description"/>
+              </div>
+
+              <div class="form-group">
+                <label for="name-input">Photo</label>
+                <input type="text" class="form-control" name="photo-input" id="photo-input" value="project.jpg"/>
+              </div>
+
+              <div class="form-group">
+                <label for="type-input">Type</label>
+                <select class="form-control" name="type-input" id="type-input">
+                  <option value="1">Painting</option>
+                  <option value="2">Sculpture</option>
+                  <option value="3">Digital</option>
+                </select>
+              </div>
+
+              <button type="submit" class="btn btn-primary">Add</button>
+            </form>
+          </div>
+
+        
+      </div>
+    );
+  }
 }
 
 export default App;
